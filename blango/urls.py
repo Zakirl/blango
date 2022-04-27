6e627afa36837809c27980f97c3acec83575ec89
Ahmed Zaki <ahmed.zaki1998@yahoo.com>
2022-04-27 21:55:37 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -21,5 +21,7 @@ import blog.views
 urlpatterns = [
     # other patterns
     path('admin/', admin.site.urls),
-    path("", blog.views.index)
+    path("", blog.views.index),
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
+
 ]
--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,6 +2,7 @@ from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from blog.models import Post
 
 from django.contrib.auth import get_user_model
 user_model = get_user_model()
@@ -30,3 +31,42 @@ def author_details(author, current_user=None):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
+    print(author)
+    print(current_user)
+    if author == current_user:
+        print('333')
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
